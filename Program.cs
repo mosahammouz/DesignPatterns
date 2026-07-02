4f9b6ba2d57a36590de5a614e8f0f0690dc12713
Mousa Hammouz <mousa.hammouz@foothillsolutions.com>
2026-07-02 14:56:40 +0300

    Singleton Example is done

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using DesignPatternsNotion;
+
+//Singleton s3 = new Singleton(); this is why constructor is private 
+Singleton s1 = Singleton.Instance; //A thread is whatever is currently running this line.
+s1.DoSomething();
+Singleton s2 = Singleton.Instance;
+Console.WriteLine($"are s1 and s2 both point to the same obj ? {ReferenceEquals(s1,s2)}");
